c50941728f814909daddab7b44aec0d8e8758d7c
rythroja <jared@mercenarytricking.com>
2024-09-06 14:17:16 -0700

    start weather app

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import data from "./data/data.xml";
 import notes from "./data/data.csv";
 // import Icon from "./images/icon.png";
 
-const element = document.createElement("div");
+// const element = document.createElement("div");
 
 // Lodash, now imported by this script
 // element.innerHTML = _.join(["Hello", "webpack"], " ");
-element.textContent = "indexjs";
-element.classList.add("hello");
+// element.textContent = "indexjs normal";
+// element.classList.add("hello");
 
 // element.appendChild(myIcon);
 
-console.log(data);
-console.log(notes);
-document.querySelector("body").appendChild(element);
+// console.log(data);
+// console.log(notes);
+// document.querySelector("body").appendChild(element);
